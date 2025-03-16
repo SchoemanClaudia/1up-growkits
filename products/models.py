e244f5c5b78de26410438cf731803f2de249a775
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-16 22:43:13 +0000

    Modify product model to track stock levels

--- products/models.py
+++ products/models.py
@@ -10,13 +10,21 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True) #admin only
-    low_stock_indicator = models.BooleanField(default=False) #admin only
+    stock_quantity = models.IntegerField(default=0)  #track stock levels
+    created_at = models.DateTimeField(auto_now_add=True)  #admin only
+    low_stock_indicator = models.BooleanField(default=False)  #admin only
 
     def save(self, *args, **kwargs):
-        # Automatically set a low stock alert
-        self.low_stock_indicator = self.stock_quantity < 5
+        """ Auto-update a low stock flag to user """
+        if self.stock_quantity == 0:
+            self.low_stock_indicator = True  #mark if out of stock
+        elif self.stock_quantity < 5:
+            self.low_stock_indicator = True  #low stock warning
+        else:
+            self.low_stock_indicator = False  #fully stock
+        
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
