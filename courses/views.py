9c02d2866e9ef865cd5a92a7e640d13e539ae117
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-17 21:42:10 +0000

    Add course views & template

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Course, Enrollment
 
 # Create your views here.
+
+def online_courses(request):
+    """ A view to show available online courses for enrollment """
+
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'courses/courses.html', context)
