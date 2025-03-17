1ef53599d847490f50796e3c7f611d1d50dc36a0
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-17 13:57:16 +0000

    Add product views & template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def mushroom_kits(request):
+    """ A view to show all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
