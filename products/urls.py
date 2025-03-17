912bd1e24249f26ea0d9081c2903a6b73a7f1c8b
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-17 20:22:05 +0000

    Add product detail functionality

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mushroom_kits, name='products')
+    path('', views.mushroom_kits, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
