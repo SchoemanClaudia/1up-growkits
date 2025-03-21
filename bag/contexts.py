93a69e00393475faceac6ceb46c1ef0ff5d98dbe
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-21 08:21:32 +0000

    Fix course display in bag view

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,33 +2,54 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from courses.models import Course
 
 def bag_contents(request):
-
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_key, quantity in bag.items():
+        # Product handler
+        if item_key.startswith("product_"):
+            item_id = item_key.split("_")[1]
+            product = get_object_or_404(Product, pk=item_id)
+            subtotal = quantity * product.price 
+            total += subtotal
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+                'subtotal': subtotal, 
+                'type': 'product',
+            })
+        # Course handler
+        elif item_key.startswith("course_"):
+            item_id = item_key.split("_")[1]
+            course = get_object_or_404(Course, pk=item_id)
+            subtotal = quantity * course.price 
+            total += subtotal
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': course,
+                'subtotal': subtotal, 
+                'type': 'course',
+            })
 
+    # Calculate delivery costs
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -1,26 +1,38 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
-
-def view_bag(request):
-    """ A view that renders the bag contents page """
-
-    return render(request, 'bag/bag.html')
-
+from django.shortcuts import render, redirect, get_object_or_404
+from courses.models import Course
+from products.models import Product
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified product or course to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    # Differentiate items
+    item_type = request.POST.get('item_type')
+
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # Determine if Product / Course
+    if item_type == "course":
+        item = get_object_or_404(Course, pk=item_id)
+        # Course item indetifier
+        item_key = f"course_{item_id}"
     else:
-        bag[item_id] = quantity
+        item = get_object_or_404(Product, pk=item_id)
+        # Product item indetifier
+        item_key = f"product_{item_id}"
+
+    if item_key in bag:
+        bag[item_key] += quantity
+    else:
+        bag[item_key] = quantity
 
     request.session['bag'] = bag
 
     return redirect(redirect_url)
 
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
