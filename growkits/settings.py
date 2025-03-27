5a6ad2ba2ce9ec9152c46a22d44dfc6a9d716449
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-27 15:39:03 +0000

    Load & transfer fullDB for deploy

--- growkits/settings.py
+++ growkits/settings.py
@@ -223,3 +223,5 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# print('CONNECTED DATABASE: ', DATABASES)
