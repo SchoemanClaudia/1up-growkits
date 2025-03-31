da611d2cb7cc3f006a83b31498b259a27f542a7a
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-03-31 17:01:22 +0100

    Complete webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -50,8 +50,11 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
-
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_key, quantity in bag.items():
                 item_type, item_id = item_key.split('_')
                 try:
