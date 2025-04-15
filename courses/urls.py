0a105cad6e7f726876cc0b2a73b1858e71422b49
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-04-15 21:32:08 +0100

    Add edit course view, url & template

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.online_courses, name='courses'),
     path('<int:course_id>', views.course_detail, name='course_detail'),
     path('add_course/', views.add_course, name='add_course'),
+    path('edit_course/<int:course_id>/', views.edit_course, name='edit_course'),
+
 ]
--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.mushroom_kits, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add_product/', views.add_product, name='add_product'),
-    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
