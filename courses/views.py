3ab7eec4cb3d8e8040f18d36c36825969616cb74
Young_Cloudwalker <claudiaschoeman1@gmail.com>
2025-04-15 22:08:17 +0100

    Modify & secure views for superusers

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Course
 from .forms import CourseForm
@@ -30,9 +31,13 @@ def course_detail(request, course_id):
 
     return render(request, 'courses/course_detail.html', context)
 
-
+@login_required
 def add_course(request):
     """ Add a course to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
     if request.method == 'POST':
         form = CourseForm(request.POST, request.FILES) 
         if form.is_valid():
@@ -49,9 +54,13 @@ def add_course(request):
     }
     return render(request, 'courses/add_course.html', context) 
 
-
+@login_required
 def edit_course(request, course_id):
     """ Edit a course in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
     course = get_object_or_404(Course, pk=course_id)
     if request.method == 'POST':
         form = CourseForm(request.POST, request.FILES, instance=course)
@@ -73,9 +82,13 @@ def edit_course(request, course_id):
 
     return render(request, template, context)
 
-
+@login_required
 def delete_course(request, course_id):
     """ Delete a course from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
     course = get_object_or_404(Course, pk=course_id)
     course.delete()
     messages.success(request, 'Course deleted!')
